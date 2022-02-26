82f2e973ed7d486acf1dddcef9dff6601371dd83
gsaqlaien <gsaqlaien@outlook.com>
2022-02-25 19:15:48 -0800

    error message added

--- js/api.practise.js
+++ js/api.practise.js
@@ -1,1 +1,57 @@
-console.log("api practise")
+const searchButton = () => {
+    document.getElementById('search-button')
+    const input = document.getElementById('input-value');
+    const error = document.getElementById('error');
+
+    const inputValue = input.value;
+
+    if (isNaN(inputValue) || inputValue == "") {
+        error.innerText = "Please place a number ";
+        input.value = "";
+    }
+    else if (inputValue <= 0) {
+        error.innerText = "Please place a positive value to get the result";
+        input.value = "";
+    }
+
+    else {
+        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
+            .then(res => res.json())
+            .then(data => cardsDisplay(data))
+        input.value = "";
+    }
+
+}
+
+const cardsDisplay = (cards) => {
+    console.log(cards)
+
+}
+
+
+// const searchButton = () => {
+//     document.getElementById('search-button');
+//     const input = document.getElementById('input-value');
+//     const error = document.getElementById('error');
+
+//     const inputValue = input.value;
+
+
+//     if (isNaN(inputValue) || inputValue == "") {
+//         error.innerText = "please place a number"
+//         error.innerText = "";
+//     }
+//     else if (inputValue <= 0) {
+//         error.innerText = "please place a positive number to get the Value";
+//         error.innerText = "";
+//     }
+
+//     else {
+//         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
+//             .then(res => res.json())
+//             .then(data => cardsDisplay(data))
+//     }
+//     const cardsDisplay = (cards) => {
+//         console.log(cards)
+//     }
+// }
